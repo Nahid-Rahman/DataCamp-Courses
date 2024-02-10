153491b625c938a7836448877f1aa7defd3e59d2
Mahmudur Rahman Nahid <52327199+Nahid-Rahman@users.noreply.github.com>
2024-02-11 01:15:30 +0600

    Add files via upload

---
+++ Cleaning Data in Python/Chapter 03 - Advanced data problems/03. Completeness/01. Missing investors.py
@@ -0,0 +1,42 @@
+#Problem 01
+
+# Print number of missing values in banking
+print(banking.isna().sum())
+
+# Visualize missingness matrix
+msno.matrix(banking)
+plt.show()
+
+
+
+#Problem 02
+
+# Print number of missing values in banking
+print(banking.isna().sum())
+
+# Visualize missingness matrix
+msno.matrix(banking)
+plt.show()
+
+# Isolate missing and non missing values of inv_amount
+missing_investors = banking[banking['inv_amount'].isna()]
+investors = banking[~banking['inv_amount'].isna()]
+
+
+
+# Problem 03
+# Print number of missing values in banking
+print(banking.isna().sum())
+
+# Visualize missingness matrix
+msno.matrix(banking)
+plt.show()
+
+# Isolate missing and non missing values of inv_amount
+missing_investors = banking[banking['inv_amount'].isna()]
+investors = banking[~banking['inv_amount'].isna()]
+
+# Sort banking by age and visualize
+banking_sorted = banking.sort_values('age')
+msno.matrix(banking_sorted)
+plt.show()
